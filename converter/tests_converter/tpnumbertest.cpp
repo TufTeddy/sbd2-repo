9f14965d47beedd6e77506c33f7ed0afb4059886
LeshaTheSlayer <muhomorcheg@gmail.com>
2019-04-08 20:04:38 +0700

    tests dir

---
+++ converter/tests_converter/tpnumbertest.cpp
@@ -0,0 +1,34 @@
+#include <QtTest>
+#include "../tpnumber.h"
+#include "../tpnumberconverter.h"
+class TPNumberTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    TPNumberTest();
+    ~TPNumberTest();
+
+private slots:
+    void test_case1();
+
+};
+
+TPNumberTest::TPNumberTest()
+{
+
+}
+
+TPNumberTest::~TPNumberTest()
+{
+
+}
+
+void TPNumberTest::test_case1()
+{
+
+}
+
+QTEST_APPLESS_MAIN(TPNumberTest)
+
+//#include "tst_firsttest.moc"
